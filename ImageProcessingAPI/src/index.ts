39889a2cbd9f46410dab22eeefc0ed88f480d0a6
saikn85 <85madmaxfury@gmail.com>
2023-11-21 10:40:15 +0530

    imageprocessingapi - refactor base

--- ImageProcessingAPI/src/index.ts
+++ ImageProcessingAPI/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    console.log(req.ip)
-    res.send('Hello World!')
-})
+    console.log(req.ip);
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log(`server started at localhost:${port}`)
-})
+    console.log(`server started at localhost:${port}`);
+});
